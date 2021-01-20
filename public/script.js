f1d80ab149c171de84233845473d78917bf62377
Glitch (hello-express) <none>
2021-01-20 09:02:02 +0000

    ⛰💧 Checkpoint ./public/script.js:27344973/7202

--- public/script.js
+++ public/script.js
@@ -114,10 +114,6 @@ function preload() {
   //this.load.atlas("tank", "https://cdn.glitch.com/4fc97b97-fbe3-4d16-be05-c0b4fb6814b8%2Fblue.png", "blue.json");
   this.load.image("tank", "https://cdn.glitch.com/4fc97b97-fbe3-4d16-be05-c0b4fb6814b8%2Fblue.png");
   this.load.json('shapes', 'blue.json');
-  /*this.load.image(
-    "tank",
-    "https://cdn.glitch.com/772bc608-91dd-4577-857d-f1f6ed4d7332%2Ftank1.png"
-  );*/
   this.load.image(
     "gun",
     "https://cdn.glitch.com/4fc97b97-fbe3-4d16-be05-c0b4fb6814b8%2Fweap_cannon.png?"
@@ -132,7 +128,6 @@ function preload() {
     "tiles",
     "https://cdn.glitch.com/4fc97b97-fbe3-4d16-be05-c0b4fb6814b8%2Fwalls.png"
   );
-  this.load.atlas("tank2", "https://cdn.glitch.com/4fc97b97-fbe3-4d16-be05-c0b4fb6814b8%2Fblue.png", "atlas.json");
 }
 
 function destroyTile (tile)
@@ -189,134 +184,10 @@ function create() {
     "collisionstart",
     function(event) {
       tzodCollision.update(event);
-      //log(event);
-//       for (var i = 0; i < event.pairs.length; i++) {
-//         var bodyA = event.pairs[i].bodyA;
-//         var bodyB = event.pairs[i].bodyB;
-//         var damagg=11;
-//         //log(event.pairs[i].bodyA.label);
-//         if (event.pairs[i].bodyA.label=="heavyBullet"){damagg=101;}
-//         /*if(bodyA.gameObject){
-//           if(bodyA.gameObject.name){
-//           log('BODY A' + bodyA.gameObject.name);
-            
-//           }
-//         }else if(bodyB.gameObject){
-//           if(bodyB.gameObject.name){
-//             log('BODY B' + bodyB.gameObject.name);
-//           }
-//         }*/
-//         var tileBody = bodyA.label === "collides" ? bodyA : bodyB;
-//         //log("a=");
-//         //log(bodyA.velocity);
-//         //log(bodyA.label);
-//         //gameObject.name
-//         //log(bodyA);
-//         //log("b=");
-//         //log(bodyB.velocity);
-//         //log(bodyB.label);
-//         //log(bodyB);
-//         if (tileBody.gameObject) {
-//           var tileWrapper = tileBody.gameObject;
-//           if (tileWrapper.tile) {
-//             var tile = tileWrapper.tile;
-//             if (tile.properties.hp!=0) {
-//               //if 
-//               for(var damag=damagg;damag>0;){
-//                 var hp= tile.properties.hp
-//                 //log("hp "+hp);
-//                 //log("dam "+damag);
-//                 if (tile.properties.hp>0){
-//                   tile.properties.hp=tile.properties.hp-damag;
-//                   damag=damag-hp
-//                 }//else if (tile.properties.hp==0){damag=-1;}
-//                 else if (tile.properties.hp<=0){
-//                   if (tile.properties.nextlauer<1){
-//                     destroyTile(tile);
-//                     damag=-1;
-//                   }else{
-//                     //destroyTile(tile);
-//                     //log("x "+tile.x+"y "+tile.y);
-//                     belowLayer.putTileAtWorldXY(tile.properties.nextlauer, tile.x*32, tile.y*32).setCollision(true);
-//                     tile.properties.hp=belowLayer.gidMap[1].tileProperties[tile.properties.nextlauer-1].hp;
-//                     tile.properties.nextlauer=belowLayer.gidMap[1].tileProperties[tile.properties.nextlauer-1].nextlauer;
-//                   }
-//                 }
-//               //log("last "+tile.properties.hp);
-//                 //log("last "+tile.properties.nextlauer);
-//               }
-              
-              
-//             }
-//           }
-//         }
-
-//         //             if ((bodyA === playerBody && bodyB.label === 'disappearingPlatform') ||
-//         //                 (bodyB === playerBody && bodyA.label === 'disappearingPlatform'))
-//         //             {
-//         //                 var tileBody = bodyA.label === 'disappearingPlatform' ? bodyA : bodyB;
-
-//         //                 // Matter Body instances have a reference to their associated game object. Here,
-//         //                 // that's the Phaser.Physics.Matter.TileBody, which has a reference to the
-//         //                 // Phaser.GameObjects.Tile.
-//         //                 var tileWrapper = tileBody.gameObject;
-//         //                 var tile = tileWrapper.tile;
-
-//         //                 // Only destroy a tile once
-//         //                 if (tile.properties.isBeingDestroyed)
-//         //                 {
-//         //                     continue;
-//         //                 }
-//         //                 tile.properties.isBeingDestroyed = true;
-
-//         //                 // Since we are using ES5 here, the local tile variable isn't scoped to this block -
-//         //                 // bind to the rescue.
-//         //                 this.tweens.add({
-//         //                     targets: tile,
-//         //                     alpha: { value: 0, duration: 500, ease: 'Power1' },
-//         //                     onComplete: destroyTile.bind(this, tile)
-//         //                 });
-//       }
-
-      // Note: the tile bodies in this level are all simple rectangle bodies, so checking the
-      // label is easy. See matter detect collision with tile for how to handle when the tile
-      // bodies are compound shapes or concave polygons.
     },
     this
   );
   
-  /*scene.input.keyboard.on('keydown-W', function(){
-    clients[id].thrust(0.03);
-  });
-  
-  scene.input.keyboard.on('keydown-A', function(){
-    clients[id].setRotation(clients[id].rotation - 0.1);
-  });
-  
-  scene.input.keyboard.on('keydown-S', function(){
-    clients[id].thrustBack(0.03);
-  });
-  
-  scene.input.keyboard.on('keydown-D', function(){
-    clients[id].setRotation(clients[id].rotation + 0.1);
-  });*/
-  
-  /*this.input.keyboard.on('keydown', function(params){
-    if (params.key=='s') {
-    //socket.emit('downisDown',{'id': id});
-    clients[id].thrustBack(0.03);
-  } else if (params.key=='w') {
-    //socket.emit('upisDown',{'id': id});
-    clients[id].thrust(0.03);
-  }
-  if (params.key=='a') {
-    //socket.emit('leftisDown',{'id': id});
-    clients[id].setRotation(clients[id].rotation - 0.1);
-  } else if (params.key=='d') {
-    //socket.emit('rightisDown',{'id': id});
-    clients[id].setRotation(clients[id].rotation + 0.1);
-  }
-  });*/
 }
 
 function handleMove() {
@@ -338,21 +209,7 @@ function update(time, delta) {
     this.cameras.main
   );*/
   this.matter.world.convertTilemapLayer(belowLayer);
-  // Draw tiles (only within the groundLayer)
-  /*if (pointer.isDown) {
-    //belowLayer.getTileAtWorldXY(worldPoint.x, worldPoint.y).setCollision(false, false, false, false, true);
-    //belowLayer.putTileAtWorldXY(1, worldPoint.x, worldPoint.y).setCollision(false);
-    //belowLayer.putTileAtWorldXY(17, worldPoint.x, worldPoint.y).setCollision(false);
-    //belowLayer.setCollisionByProperty({ collides: false });
-    //const debugGraphics = this.add.graphics().setAlpha(0);
-    //belowLayer.renderDebug(debugGraphics, {});
-    //log(Phaser.Physics.Matter.TileBody.destroy());
-    //this.Physics.Matter.TileBody.destroy()
-    //this.matter.world.convertTilemapLayer(belowLayer);
-    /*belowLayer.gidMap[1].tileProperties[16].hp=5
-    log(belowLayer.gidMap[1].tileProperties[16].hp);
-    log(belowLayer);*/
-  //}
+
   if (pointer.isDown) {
     clients[id].fire();
     socket.emit("fire", {
@@ -371,13 +228,6 @@ function update(time, delta) {
     //console.log("+");
   }
 
-  /*if (){
-    
-  }else if (gun.rotation>Math.atan2(pointer.y - gun.y, pointer.x - gun.x))
-    {
-      
-    }*/
-  //gun.rotation=Math.atan2(pointer.y - gun.y, pointer.x - gun.x)
   if (cursors.S.isDown) {
     //socket.emit('downisDown',{'id': id});
     clients[id].thrustBack(0.03);
@@ -393,29 +243,3 @@ function update(time, delta) {
     clients[id].setRotation(clients[id].rotation + 0.1);
   }
 }
-
-/*function createTank(game, id, x, y) {
-  log(id);
-  if (!(id in clients)) {
-    
-    // тут создать танк игрока 2
-    //clients[id] = game.add.sprite(64, 64, "tank");
-    /*gun[id] = game.add.image(0, 0, "gun");
-game.matter.add.gameObject(clients[id]).setScale(0.8,0.8);
-game.matter.add.gameObject(gun[id]);
-
-  cat1 = game.matter.world.nextCategory();
-  cat2 = game.matter.world.nextCategory();
-  cat3 = game.matter.world.nextCategory();
-
-  //player.setRotation(45);
-  clients[id].setFrictionAir(0.5);
-  clients[id].setPosition(x, y);
-  gun[id].depth = 1;
-  clients[id].setMass(5);
-  clients[id].setCollisionCategory(cat1);
-  gun[id].setCollisionCategory(cat2);
-  gun[id].setCollidesWith(cat2);
-  game.matter.add.constraint(clients[id], gun[id], 0, 0);*/
-  //}*/
-//}
