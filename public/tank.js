0e082244819882837043282c292b573263e5cfd0
Glitch (hello-express) <none>
2021-05-16 18:04:57 +0000

    👚🐷 Checkpoint ./public/tank.js:27314738/52

--- public/tank.js
+++ public/tank.js
@@ -188,10 +188,10 @@ class tank extends Phaser.Physics.Matter.Sprite {
     this.nickname.setScrollFactor(0);
     log(this.nickname);
     var that = this;
-    /*this.updater = scene.matter.world.on('beforeupdate', function(time, delta){//ник за игроком
+    this.updater = scene.matter.world.on('beforeupdate', function(time, delta){//ник за игроком
       that.nickname.x = that.x - 25;
     that.nickname.y = that.y - 50;
-    }, this);*/
+    }, this);//*/
   }
   
   setNick(text){
@@ -210,6 +210,7 @@ class tank extends Phaser.Physics.Matter.Sprite {
   }
 
   kill() {
+    logObj("this.nickname", this.nickname);
     log(this.joint);
     this.gun.destroy();
     delete this.gun;
